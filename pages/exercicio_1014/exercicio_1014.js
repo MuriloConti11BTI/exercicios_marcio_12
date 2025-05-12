0e62f5cbfabb91a12ed96e5e35ee2f2a7426d7b1
mcontilp <mcontilp@gmail.com>
2025-05-12 13:58:46 +0000

    criação da conexão entre as páginas do index.html e os exercícios 1011 e 1014, conclusão do exercício 1014

---
+++ pages/exercicio_1014/exercicio_1014.js
@@ -0,0 +1,5 @@
+let consumoMedio = (x, y) => x / y
+
+console.log(`${consumoMedio(500, 35).toFixed(3)} km/l`)
+console.log(`${consumoMedio(2254, 124.4).toFixed(3)} km/l`)
+console.log(`${consumoMedio(4554, 464.6).toFixed(3)} km/l`)
